4ae1b2e5f279b1ce221f126178a033bfc713fa92
Li HN <lhn19990113@gmail.com>
2022-05-02 20:03:32 +0800

    Improve the measurement approach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from net import NN
 from utils import *
+from parameters import *
 import numpy as np
 import torch
 from torch import optim
@@ -27,43 +28,61 @@ def data_generation():
 
 
 def train(net):
-    loss_rec = []
-    step = []
-    for i in range(5000):
-        train, label = data_generation()
-        optimizer.zero_grad()
-        output = net(train)
-        mse = torch.nn.MSELoss()
-        loss = mse(output, label)
-        loss_rec.append(loss.item())
-        step.append(i)
-        loss.backward()
-        optimizer.step()
+    train, label = data_generation()
+    optimizer.zero_grad()
+    output = net(train)
+    mse = torch.nn.MSELoss()
+    loss = mse(output, label)
+    loss.backward()
+    optimizer.step()
 
-    return step, loss_rec
+    return loss.item()
 
 
 def test(net):
     correct = 0
-    for i in range(1000):
+    for i in range(TEST_PARAMS.TEST_STEPS):
         train, label = data_generation()
         output = net(train)
         actual = tensor_to_item(torch.argmax(label, dim=0))
         predict = tensor_to_item(torch.argmax(output, dim=0))
         if predict == actual:
             correct += 1
-    accuracy = correct / 1000
+    accuracy = correct / TEST_PARAMS.TEST_STEPS
     print(accuracy * 100, '%')
+    return accuracy
 
 
 if __name__ == '__main__':
     net = NN(10, 2)
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
 
-    step, loss_rec = train(net)
-    test(net)
+    loss_recs = []
+    acc_recs = []
+    training_steps = []
+    testing_steps = []
+    training_step = 0
+    for episode in range(TRAIN_PARAMS.EPISODE):
+        for i in range(TRAIN_PARAMS.TRAINING_STEPS):
+            loss_rec = train(net)
+            loss_recs.append(loss_rec)
+            training_steps.append(training_step)
+            training_step += 1
+        accuracy = test(net)
+        acc_recs.append(accuracy)
+        testing_steps.append(episode)
 
-    plt.figure()
-    plt.plot(step, loss_rec, color='blue', label='Loss')
+    plt.figure(figsize=(10, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(training_steps, loss_recs, color='blue')
+    plt.xlabel('Steps', fontsize=12)
+    plt.ylabel('Loss', fontsize=12)
+
+    plt.subplot(1, 2, 2)
+    plt.plot(testing_steps, acc_recs, color='red')
+    plt.xlabel('Episodes', fontsize=12)
+    plt.ylabel('Accuracy', fontsize=12)
+
+    plt.tight_layout()
     plt.show()
 
---
+++ parameters.py
@@ -0,0 +1,13 @@
+import numpy as py
+import torch
+
+
+class TRAIN_PARAMS:
+    L_R = 0.01
+    MOMENTUM = 0.5
+    TRAINING_STEPS = 10
+    EPISODE = 500
+
+
+class TEST_PARAMS:
+    TEST_STEPS = 100
